79b999739d62356804b5ac91bba5384eed71ecfe
nmcarp99 <nmcarp99@gmail.com>
2020-12-03 08:28:04 -0500

    initial commit

---
+++ main.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Learn_Colemak
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            while (Console.ReadKey().Key != ConsoleKey.Escape) {
+            }
+        }
+    }
+}
