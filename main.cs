e8b39af17b37b9ce3e7e8afdac98d2a0fc2a8186
nmcarp99 <nmcarp99@gmail.com>
2020-12-03 09:21:18 -0500

    works

--- main.cs
+++ main.cs
@@ -2,12 +2,31 @@
 
 namespace Learn_Colemak
 {
-    class main
-    {
-        static void Main(string[] args)
-        {
-            while (Console.ReadKey().Key != ConsoleKey.Escape) {
-            }
-        }
-    }
+	class main
+	{
+		static void Main(string[] args)
+		{
+			int numCorrect = 0;
+			int numIncorrect = 0;
+			char currentChar = ' ';
+			char currentCorrectChar = args[new Random().Next(args.Length)][0];
+			ConsoleKeyInfo currentKey = new ConsoleKeyInfo();
+			Console.WriteLine("Ready: ");
+			while (currentKey.Key != ConsoleKey.Escape) {
+				currentChar = ' ';
+				currentCorrectChar = args[new Random().Next(args.Length)][0];
+				while (currentKey.Key != ConsoleKey.Escape && currentChar != currentCorrectChar) {
+					Console.Clear();
+					Console.WriteLine($"Correct: {numCorrect}  Incorrect: {numIncorrect}  {currentCorrectChar}");
+					currentKey = Console.ReadKey();
+					currentChar = currentKey.KeyChar;
+					if (currentChar == currentCorrectChar) {
+						numCorrect++;
+					} else {
+						numIncorrect++;
+					}
+				}
+			}
+		}
+	}
 }
